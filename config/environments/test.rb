c3ec05725f108b839fc0d476914229250c7c9faf
grosendo2006 <gonzalo.rosendo@emeritus.org>
2024-07-29 12:56:04 -0300

    add header navigation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV['CI'].present?
 
+  # Compress CSS using a preprocessor.
+  config.assets.css_compressor = nil
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
